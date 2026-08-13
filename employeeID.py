b9b303aab564bfcb969763eeb5b666ddcef25996
rohanbajantri82 <rohanbajantri82@gmail.com>
2026-08-13 16:12:01 +0530

    Add files via upload

---
+++ employeeID.py
@@ -0,0 +1,10 @@
+name = input("Enter Employee Name: ")
+role = input("Enter Employee Role: ")
+salary = float(input("Enter Monthly Salary: "))
+
+print("\n---------------")
+print("EMPLOYEE IDENTITY CARD")
+print("Name   : ",name)
+print("Role   : ", role)
+print("Salary : ₹",salary)
+
