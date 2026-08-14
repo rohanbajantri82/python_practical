c8653a10b9fb0ca7dd88a13de488eb863825306c
rohanbajantri82 <rohanbajantri82@gmail.com>
2026-08-14 13:35:23 +0530

    Add files via upload

--- lab6.py
+++ lab6.py
@@ -1,3 +1,4 @@
+print("-----star pattern-----")
 rows=7
 cols=30
 
@@ -19,26 +20,26 @@ rows=5
 
 for i in range(1,rows+1):
     for j in range(1,6):
-             print(j,end="")
+             print(j,end=" ")
     print()
 
 rows=5
 for i in range(1,rows+1):
     for j in range(1,i+1):
-             print(j,end="")
+             print(j,end=" ")
     print()
 
 rows=5
 for i in range(1,rows+1):
     for j in range(i):
-             print("*",end="")
+             print("*",end=" ")
     print()
 
 rows=5
 
 for i in range(rows,0,-1):
     for j in range(i):
-             print("*",end="")
+             print("*",end=" ")
     print()
 
                 
