dbf39cd9a38a64609c42ec4a1a54793b183676d1
rohanbajantri82 <rohanbajantri82@gmail.com>
2026-09-10 15:01:36 +0530

    Add files via upload

---
+++ pr5ex1.py
@@ -0,0 +1,12 @@
+# Accept the number of rows from the user
+rows = int(input("Enter the number of rows: "))
+
+print("\n--- Right-Angled Triangle Pattern ---")
+
+
+for i in range(1, rows + 1):
+    
+    for j in range(1, i + 1):
+        print(i, end=" ")
+   
+    print()
